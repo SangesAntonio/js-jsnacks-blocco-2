7cddfac1f2bad4aaf72f8d1aab25cf8ed1fd2815
SangesAntonio <sanges65@gmail.com>
2022-01-13 17:45:32 +0100

    added script4

--- js/script4.js
+++ js/script4.js
@@ -3,7 +3,15 @@ e fai la somma di tutti gli elementi che sono in posizione dispari*/
 
 
 
-const numbers=['23','44','88','76','91','35'];
+const numb=20;
+const numbers=[];
+let r= 0;
+while(r < numb){
+    let rndNumb=Math.floor(Math.random() *200);
+    numbers.push(rndNumb);
+    r++;
+}
+console.log(numbers)
 
 
 
