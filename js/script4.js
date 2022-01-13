31ef432df87e5bdd95c6e27ce82a405e475e0566
SangesAntonio <sanges65@gmail.com>
2022-01-13 23:24:57 +0100

    added script 5

--- js/script4.js
+++ js/script4.js
@@ -2,23 +2,30 @@
 e fai la somma di tutti gli elementi che sono in posizione dispari*/
 
 
-
-const numb=20;
+//calcolo l'array di numeri random
+let numb=20;
 const numbers=[];
 let r= 0;
+let sum=0;
 while(r < numb){
     let rndNumb=Math.floor(Math.random() *200);
     numbers.push(rndNumb);
+
+    //sommo i numeri in posizione dispari
+    if(r % 2 !== 0){
+        sum +=numbers[r]
+    }
+  
     r++;
 }
 console.log(numbers)
-
-let sum;
-
-while(){
+console.log(sum)
 
 
-}
+
+
+
+
 
 
 
